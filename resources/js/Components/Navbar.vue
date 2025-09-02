bc2e8984f1c544e40b4f0b1e04b83f0cff73fffb
tahir <nestandart.mda@gmail.com>
2025-09-02 20:16:54 +0300

    сделал страницу About

---
+++ resources/js/Components/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav class="bg-gray-100/50 backdrop-blur-sm p-4 flex items-center">
+        <a href="#" class="text-xl font-bold text-gray-800">
+            <img src="https://erp.f4st.dev/assets/images/logo.svg" alt="logo">
+        </a>
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,11 +1,28 @@
 <script setup>
-
+import Navbar from "@/Components/Navbar.vue";
 </script>
 
 <template>
-123123
+    <div class="bg-[url('https://cdn.radiosputnik.ru/images/148340/76/1483407615_0:386:7360:4526_1920x1080_80_0_0_a4a83e018bd75d8e99c6c66b3eedd954.jpg')] bg-no-repeat bg-cover bg-center h-screen w-screen flex flex-col items-center justify-center">
+
+        <div class="absolute top-0 left-0 w-full z-10">
+            <Navbar/>
+        </div>
+
+        <div class="bg-white/50 backdrop-blur-sm p-6 rounded-lg shadow-lg max-w-3xl mx-auto my-auto w-11/12 md:w-1/2">
+            <h2 class="text-3xl font-bold mb-4 text-gray-800">О нас</h2>
+            <p class="text-gray-800 leading-relaxed mb-4">
+                Миссия <strong class="text-blue-700">F4studio</strong> — приносить радость и новые эмоции через интерактивные развлечения.
+            </p>
+            <p class="text-gray-800 leading-relaxed mb-4">
+                Мы создаём собственные браузерные игры, стремясь сделать их максимально интересными и доступными для каждого. Мы работаем над тем, чтобы наши игры были не просто способом убить время, а источником позитива и вдохновения.
+            </p>
+            <p class="text-gray-800 leading-relaxed">
+                Используя наш опыт в разработке, мы также принимаем заказы на создание небольших промо-сайтов для рекламных кампаний. Мы знаем, как важно быстро и эффективно донести информацию до аудитории, и готовы помочь вам в этом.
+            </p>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-
 </style>
